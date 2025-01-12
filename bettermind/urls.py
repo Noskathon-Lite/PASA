d372fbcd4192531b38d1188e28f39903c4e4429d
sandarva3 <sandarvapoudel182@gmail.com>
2025-01-12 15:28:33 +0545

    endopints

--- bettermind/urls.py
+++ bettermind/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import(
-    test_view
+
+
+from .views import (
+    UserRegistrationAPIView,
+    ProfRegistrationAPIView,
+    LoginAPIView
 )
 
 urlpatterns = [
-    path("", test_view, name="test")
+    path('register/user', UserRegistrationAPIView.as_view(), name='userRegistration'),
+    path('register/prof', ProfRegistrationAPIView.as_view(), name="profRegistration"),
+    path('login', LoginAPIView.as_view(), name="login"),
 ]
