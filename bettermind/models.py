f31ff2dcb7d50542d9a937f5737c5bc05eba1008
sandarva3 <sandarvapoudel182@gmail.com>
2025-01-12 14:21:50 +0545

    user models developed

--- bettermind/models.py
+++ bettermind/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    fullname = models.CharField(max_length=45)
+    USER_TYPE_CHOICES = (
+        ('user', 'User'),
+        ('prof', 'Prof'),
+    )
+    user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES, default='user')
+
+
+class User(CustomUser):
+    best_fit_profs = models.ManyToManyField(
+        'Prof',
+        related_name= 'matched_by_user',
+        blank=True
+    )
+
+    def __str__(self):
+        return self.username
+    
+
+
+class Prof(CustomUser):
+    verified = models.BooleanField(default=False)
+    best_fit_users = models.ManyToManyField(
+        'User',
+        related_name='matched_by_profs',
+        blank=True
+    )
+
+    def __str__(self):
+        return self.username
