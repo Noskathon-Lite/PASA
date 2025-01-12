d166a16a86471f3624d130623cbd84ef486ce616
sandarva3 <sandarvapoudel182@gmail.com>
2025-01-12 14:58:18 +0545

    api endpoints. login/registration

--- bettermind/serializers.py
+++ bettermind/serializers.py
@@ -5,11 +5,9 @@ from django.contrib.auth import authenticate
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
-
     class Meta:
         model = User
         fields = ['username', 'email', 'password', 'fullname']
-
     def create(self, validated_data):
         print("User Serializer")
         user = User.objects.create_user(
@@ -18,17 +16,14 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
             password=validated_data['password'],
             fullname=validated_data['fullname']
         )
-
         return user
     
 
 class ProfRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
-
     class Meta:
         model = Prof
         fields = ['username', 'email', 'password', 'fullname']
-
     def create(self, validated_data):
         print("Prof Serializer")
         prof = Prof.objects.create_user(
@@ -38,5 +33,25 @@ class ProfRegistrationSerializer(serializers.ModelSerializer):
             fullname=validated_data['fullname'],
             user_type = 'prof'
         )
-
         return prof
+
+
+class LoginAPIView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            print(f"Logged in as: {user.username}")
+            #print(f"ATTRIBUTES: {user.__dict__}") To get all attributes of an object.
+            return Response({
+                "msg": "Login successful",
+                "username": user.username,
+                "userType": "Prof" if (user.user_type == "prof") else "User"
+            }, status=status.HTTP_200_OK)
+        else:
+            return Response(
+                {'msg': 'Invalid Username or password',
+                'error': serializer.errors
+                }, 
+                status=status.HTTP_400_BAD_REQUEST
+                )
--- bettermind/views.py
+++ bettermind/views.py
@@ -1,4 +1,49 @@
-from django.shortcuts import render, HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import UserRegistrationSerializer, ProfRegistrationSerializer, LoginSerializer
 
-def test_view(request):
-    return HttpResponse("WELCOME")
+
+class UserRegistrationAPIView(APIView):
+    def post(self, request):
+        print("WORKED")
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            print(f"User Id is: {user.id}")
+            print(f"The username is {user.username}")
+            return Response({"msg": "User registered successfully"}, status=status.HTTP_201_CREATED)
+        return Response({'message': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class ProfRegistrationAPIView(APIView):
+    def post(self, request):
+        print("WORKED")
+        serializer = ProfRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            prof = serializer.save()
+            print(f"Prof Id is: {prof.id}")
+            print(f"The username is {prof.username}")
+            return Response({"msg": "Prof registered successfully"}, status=status.HTTP_201_CREATED)
+        return Response({'message': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginAPIView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            print(f"Logged in as: {user.username}")
+            #print(f"ATTRIBUTES: {user.__dict__}") To get all attributes of an object.
+            return Response({
+                "msg": "Login successful",
+                "username": user.username,
+                "userType": "Prof" if (user.user_type == "prof") else "User"
+            }, status=status.HTTP_200_OK)
+        else:
+            return Response(
+                {'msg': 'Invalid Username or password',
+                'error': serializer.errors
+                }, 
+                status=status.HTTP_400_BAD_REQUEST
+                )
